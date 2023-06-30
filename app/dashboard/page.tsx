55887513457c06204243a2f798bb0899386afd87
mdchad <irsyad.muhd@gmail.com>
2023-06-30 14:19:53 +0800

    refactor dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,13 +12,13 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { CalendarDateRangePicker } from "@/app/examples/dashboard/components/date-range-picker"
-import { MainNav } from "@/app/examples/dashboard/components/main-nav"
-import { Overview } from "@/app/examples/dashboard/components/overview"
-import { RecentSales } from "@/app/examples/dashboard/components/recent-sales"
-import { Search } from "@/app/examples/dashboard/components/search"
-import TeamSwitcher from "@/app/examples/dashboard/components/team-switcher"
-import { UserNav } from "@/app/examples/dashboard/components/user-nav"
+import { CalendarDateRangePicker } from "@/components/date-range-picker"
+import { MainNav } from "@/components/main-nav"
+import { Overview } from "@/components/overview"
+import { RecentSales } from "@/components/recent-sales"
+import { Search } from "@/components/search"
+import TeamSwitcher from "@/components/team-switcher"
+import { UserNav } from "@/components/user-nav"
 import {useRef} from "react";
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
@@ -94,47 +94,18 @@ export default function DashboardPage() {
 
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/dashboard-light.png"
-          width={1280}
-          height={866}
-          alt="Dashboard"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/dashboard-dark.png"
-          width={1280}
-          height={866}
-          alt="Dashboard"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden flex-col md:flex">
-        <div className="border-b">
-          <div className="flex h-16 items-center px-4">
-            <TeamSwitcher />
-            <MainNav className="mx-6" />
-            <div className="ml-auto flex items-center space-x-4">
-              <Search />
-              <UserNav />
-            </div>
-          </div>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="picture">Picture</Label>
+          <Input
+            id="picture"
+            type="file"
+            {...register("file")}
+          />
         </div>
-        <div className="flex-1 space-y-4 p-8 pt-12">
-          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-            <div className="grid w-full max-w-sm items-center gap-1.5">
-              <Label htmlFor="picture">Picture</Label>
-              <Input
-                id="picture"
-                type="file"
-                {...register("file")}
-              />
-            </div>
-            <Button type="submit">Submit</Button>
-          </form>
-
-          {/*  <div className="flex items-center justify-between space-y-2">*/}
+        <Button type="submit">Submit</Button>
+      </form>
+        {/*  <div className="flex items-center justify-between space-y-2">*/}
         {/*    <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>*/}
         {/*    <div className="flex items-center space-x-2">*/}
         {/*      <CalendarDateRangePicker />*/}
@@ -237,8 +208,6 @@ export default function DashboardPage() {
         {/*      </div>*/}
         {/*    </TabsContent>*/}
         {/*  </Tabs>*/}
-        </div>
-      </div>
     </>
   )
 }
